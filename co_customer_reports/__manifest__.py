96b128d863bcae2895d08da61328019beb6c0fae
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-05-06 12:41:14 -0500

    add

---
+++ co_customer_reports/__manifest__.py
@@ -0,0 +1,35 @@
+###############################################################################################
+#
+# Luis Felipe Paternina  - Julian Bocanegra                                
+# Odoo Dev                 Odoo Consulting
+# 
+# Bogotá,Colombia
+#
+#
+###############################################################################################
+
+{
+    'name': 'co customer reports',
+
+    'version': '14.0.0.0',
+
+    'author': "Coondev S.A.S",
+
+    'contributors': ['Luis Felipe Paternina'],
+
+    'website': "www.coodev.com",
+
+    'category': 'reports',
+
+    'depends': [
+
+        'account_accountant',
+        'base',
+    ],
+
+    'data': [
+    
+        'reports/invoice_report.xml',      
+    ],
+    'installable': True
+}
